b31b0678616430cf6abd124a31c436a6643f66c4
Vince Knight <vincent.knight@gmail.com>
2018-04-01 08:24:29 +0000

    Include collection date to data collection.

--- data/main.py
+++ data/main.py
@@ -2,6 +2,7 @@
 Script to write data from @dog_rates to file.
 """
 import csv
+import datetime
 
 import pandas as pd
 import tweepy
@@ -13,7 +14,7 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 datafile_path = "./main.csv"
-columns = ["created_at", "full_text", "like_count", "retweet_count"]
+columns = ["collected_at", "created_at", "full_text", "like_count", "retweet_count"]
 
 df = pd.read_csv(datafile_path, header=None, names=columns)
 last_tweet_date = pd.to_datetime(df["created_at"]).max()
@@ -24,10 +25,12 @@ with open(datafile_path, "a") as f:
     for tweet in tweepy.Cursor(api.user_timeline, 
                                tweet_mode="extended", 
                                screen_name='@dog_rates').items():
-        csv_writer.writerow([tweet.created_at, 
+        now = str(datetime.datetime.now())
+        csv_writer.writerow([now,
+                             tweet.created_at, 
                              tweet.full_text, 
                              tweet.favorite_count, 
                              tweet.retweet_count])
         if tweet.created_at > last_tweet_date or last_tweet_date is pd.NaT:
             num_new_tweets += 1
-print(f"{num_new_tweets} new tweets collected since {last_tweet_date}")
+print(f"{now}: {num_new_tweets} new tweets collected since {last_tweet_date}")
