ee390ed9aa095614dff9ca26b7aec5786c8946c8
Vince Knight <vincent.knight@gmail.com>
2018-04-04 14:21:36 +0000

    Make collection script collect from 3 different accounts.
    Also collect follower numbers.

--- data/main.py
+++ data/main.py
@@ -4,33 +4,43 @@ Script to write data from @dog_rates to file.
 import csv
 import datetime
 
-import pandas as pd
-import tweepy
+def main(datafile_path, account, api):
+    """
+    Get all tweets from account and write to a given file.
+    """
 
-from secrets import *
+    with open(datafile_path, "a") as f:
+        csv_writer = csv.writer(f)
+        
+        user = api.get_user(account)
+        follower_count = user.followers_count
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+        for tweet in tweepy.Cursor(api.user_timeline, 
+                                   tweet_mode="extended", 
+                                   screen_name=account).items():
+            now = str(datetime.datetime.now())
 
-datafile_path = "./main.csv"
-columns = ["collected_at", "created_at", "full_text", "like_count", "retweet_count"]
+            csv_writer.writerow([account,
+                                 now,
+                                 tweet.created_at, 
+                                 tweet.id_str,
+                                 tweet.full_text, 
+                                 tweet.favorite_count, 
+                                 tweet.retweet_count,
+                                 follower_count])
 
-df = pd.read_csv(datafile_path, header=None, names=columns)
-last_tweet_date = pd.to_datetime(df["created_at"]).max()
-num_new_tweets = 0
+if __name__ == "__main__":
+    import tweepy
 
-with open(datafile_path, "a") as f:
-    csv_writer = csv.writer(f)
-    for tweet in tweepy.Cursor(api.user_timeline, 
-                               tweet_mode="extended", 
-                               screen_name='@dog_rates').items():
-        now = str(datetime.datetime.now())
-        csv_writer.writerow([now,
-                             tweet.created_at, 
-                             tweet.full_text, 
-                             tweet.favorite_count, 
-                             tweet.retweet_count])
-        if tweet.created_at > last_tweet_date or last_tweet_date is pd.NaT:
-            num_new_tweets += 1
-print(f"{now}: {num_new_tweets} new tweets collected since {last_tweet_date}")
+    from secrets import *
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    datafile_path = "./main.csv"
+
+    for account in ("@dog_rates",  "@dog_feelings", "@matt___nelson"):
+        main(datafile_path=datafile_path,
+             account=account,
+             api=api)
